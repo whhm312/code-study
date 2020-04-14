8796fc5e3fdd083439d1943cb8be1053bb7a24b0
whhm312 <whily312@gmail.com>
2020-04-14 12:04:02 +0900

    푸는 중인 문제 올라간거 초기화.

--- src/practices/programmers/level2/Solution49993.java
+++ src/practices/programmers/level2/Solution49993.java
@@ -6,18 +6,18 @@ import org.junit.Test;
 
 public class Solution49993 {
 	public int solution(String skill, String[] skill_trees) {
-		String[] skills = skill.split("");
-		String skill_tree = null;
-		for (int i = 0; i < skill_trees.length; i++) {
-			skill_tree = skill_trees[i];
-
-			for (int sk = 0; sk < skills.length; sk++) {
-				if (skill_tree.indexOf(skills[sk]) > -1) {
-					skill_tree = skill_tree.substring(skill_tree.indexOf(skills[sk]));
-				}
-			}
-
-		}
+//		String[] skills = skill.split("");
+//		String skill_tree = null;
+//		for (int i = 0; i < skill_trees.length; i++) {
+//			skill_tree = skill_trees[i];
+//
+//			for (int sk = 0; sk < skills.length; sk++) {
+//				if (skill_tree.indexOf(skills[sk]) > -1) {
+//					skill_tree = skill_tree.substring(skill_tree.indexOf(skills[sk]));
+//				}
+//			}
+//
+//		}
 		int answer = 0;
 		return answer;
 	}
