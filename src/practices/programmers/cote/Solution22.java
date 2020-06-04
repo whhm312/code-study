180c90c3b9b2cef9ca0764693a2a3a64d1d08a4f
hyemin <whily312@naver.com>
2020-06-04 21:32:52 +0900

    - 프로그래머스 코딩 테스트

---
+++ src/practices/programmers/cote/Solution22.java
@@ -0,0 +1,45 @@
+package practices.programmers.cote;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.junit.Test;
+
+public class Solution22 {
+	public long solution(long n) {
+		long answer = 0;
+		long temp = n;
+
+		Queue<Long> queue = new LinkedList<Long>();
+		while (temp != 0) {
+			queue.add(temp % 2);
+			temp /= 2;
+		}
+
+		temp = 1;
+		while (!queue.isEmpty()) {
+			answer += queue.peek() * temp;
+			temp *= 3;
+
+			queue.poll();
+		}
+
+		return answer;
+	}
+
+	@Test
+	public void test1() {
+		long expected = 9;
+		long n = 4;
+		assertEquals(expected, solution(n));
+	}
+
+	@Test
+	public void test2() {
+		long expected = 31;
+		long n = 11;
+		assertEquals(expected, solution(n));
+	}
+}
---
+++ src/practices/programmers/cote/Solution31.java
@@ -0,0 +1,8 @@
+package practices.programmers.cote;
+
+public class Solution31 {
+	public int[] solution(int total_sp, int[][] skills) {
+		int[] answer = {};
+		return answer;
+	}
+}
