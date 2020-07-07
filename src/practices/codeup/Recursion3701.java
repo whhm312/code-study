6d9d6c82baacd17c84e8229dc256fb757a7eecf5
hyemin <whily312@naver.com>
2020-07-07 11:56:28 +0900

    문제 풀이
    파스칼의 삼각형 1
    파스칼으 삼각형 2

--- src/practices/codeup/Recursion3701.java
+++ src/practices/codeup/Recursion3701.java
@@ -1,48 +1,39 @@
 package practices.codeup;
 
-import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Recursion3701 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int r = sc.nextInt();
-		int c = sc.nextInt();
+		int n = sc.nextInt();
 		sc.close();
 
-		long[][] array = new long[r][c];
-		for (int i = 0; i < 1; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				array[i][j] = 1;
-			}
-		}
+		long[][] array = createPascal(n);
 
-		for (int i = 1; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				for (int k = 0; k < j + 1; k++) {
-					array[i][j] = array[i][j] + array[i - 1][k];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (array[i][j] > 0) {
+					System.out.print(array[i][j] + " ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+
+	private static long[][] createPascal(int n) {
+		long[][] array = new long[n][n];
+		array[0][0] = 1;
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				if (j == 0) {
+					array[i][j] = array[i - 1][j];
+				} else {
+					array[i][j] = array[i - 1][j] + array[i - 1][j - 1];
 				}
 			}
 		}
 
-		BigInteger sum = BigInteger.ZERO;
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				System.out.print(array[i][j] + " ");
-				sum = sum.add(BigInteger.valueOf(array[i][j]));
-			}
-			System.out.println("");
-		}
-
-		System.out.println(sum);
-		System.out.println(sum.mod(BigInteger.valueOf(100000000)));
-
-		for (int i = 0; i < array.length; i++) {
-			sum.add(BigInteger.valueOf(Arrays.stream(array[i]).sum()));
-		}
-		System.out.println(sum);
-		System.out.println(sum.mod(BigInteger.valueOf(100000000)));
+		return array;
 	}
 }
--- src/practices/codeup/Recursion3702.java
+++ src/practices/codeup/Recursion3702.java
@@ -1,39 +1,52 @@
 package practices.codeup;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
 import java.util.Scanner;
+
+import org.junit.Test;
 
 public class Recursion3702 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		int r = sc.nextInt();
+		int c = sc.nextInt();
 		sc.close();
 
-		long[][] array = createPascal(n);
-
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				if (array[i][j] > 0) {
-					System.out.print(array[i][j] + " ");
-				}
-			}
-			System.out.println("");
-		}
+		BigInteger result = get(r, c);
+		System.out.println(result);
 	}
 
-	private static long[][] createPascal(int n) {
-		long[][] array = new long[n][n];
-		array[0][0] = 1;
-		for (int i = 1; i < n; i++) {
-			for (int j = 0; j < i + 1; j++) {
-				if (j == 0) {
-					array[i][j] = array[i - 1][j];
-				} else {
-					array[i][j] = array[i - 1][j] + array[i - 1][j - 1];
+	private static BigInteger get(int r, int c) {
+		BigInteger[][] array = new BigInteger[r][c];
+		for (int i = 0; i < 1; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = BigInteger.ONE;
+			}
+		}
+
+		for (int i = 1; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				for (int k = 0; k < j + 1; k++) {
+					if (array[i][j] == null) {
+						array[i][j] = array[i - 1][k];
+					} else {
+						array[i][j] = array[i][j].add(array[i - 1][k]);
+					}
 				}
 			}
 		}
 
-		return array;
+		return array[r - 1][c - 1].mod(BigInteger.valueOf(100000000));
+	}
+
+	@Test
+	public void test1() {
+		BigInteger expected = BigInteger.valueOf(18389602);
+		int r = 36;
+		int c = 37;
+		assertEquals(expected, get(r, c));
 	}
 }
